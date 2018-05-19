5dc723d94f61c475a8aa9b0d1b98096554a603c7
Elizabeth Chesebrough <e.chesebrough@gmail.com>
2018-05-19 17:20:40 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,3 @@
-board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,5 +6,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+board = [" "," "," "," "," "," "," "," "," "]
 
 display_board(board)
